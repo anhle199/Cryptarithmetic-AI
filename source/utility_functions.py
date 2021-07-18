392e705d2d226488ebf9c9ef7618379ab584eb7e
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-07-19 00:31:55 +0700

    implemented (no testing) backtracking function

--- source/utility_functions.py
+++ source/utility_functions.py
@@ -1,14 +1,16 @@
+import copy
+
 def degree_heuristic(csp):
     var = None
     count_constraint = 0
 
-    for key, value in csp['variables_in_operand'].items():
+    for key, value in csp['operands'].items():
         if not value['visited'] and value['count_constraint'] > count_constraint:
             var = key
             count_constraint = value['count_constraint']
 
     if var != None:
-        csp['variables_in_operand'][var]['visited'] = True
+        csp['operands'][var]['visited'] = True
 
     return var
 
@@ -25,9 +27,41 @@ def inference(assignment, csp, var, val):
 
             if (
                 len(csp['domains'][variable]) == 0 and
-                not csp['variables_in_operand'][variable]['visited']
+                not csp['operands'][variable]['visited']
             ):
                 status = False
                 break
 
     return (domains_removed, status)
+
+# def isFull(assignment, csp)
+# def sum_cryptarithmetic(data, assignment)
+
+def backtracking(data, assignment, csp, var):
+    if var == None:
+        return isFull(csp, assignment) and sum_cryptarithmetic(data, assignment):
+    else:
+        csp['operands'][var]['visited'] = True
+        domains = copy.deepcopy(csp['domains'][var])  # set
+
+        for val in domains:
+            if val not in assignment.values():
+                assignment[var] = val
+                (domains_removed, status) = inference(assignment, csp, var, val)
+
+                if status:
+                    next_var = degree_heuristic(csp)
+                    clone_domain_next_char = copy.deepcopy(csp['domains'][next_var])
+                    result = backtracking(data, assignment, csp, next_var)
+                    csp['domains'][next_var] = copy.deepcopy(clone_domain_next_char)
+
+                    if result:
+                        return result
+
+                del assignment[var]
+                for key, value in domains_removed.items():
+                    if key != var:
+                        csp['domains'][key].add(value)
+
+        csp['operands'][var]['visited'] = False
+        return False
