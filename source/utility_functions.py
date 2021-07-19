7322a457ca1500aba2c26206d9f0dc432a08a6d0
NguyenHuaHung <63572298+huahungnguyen121@users.noreply.github.com>
2021-07-19 22:51:55 +0700

    write_file function - done

--- source/utility_functions.py
+++ source/utility_functions.py
@@ -1,16 +1,17 @@
+# Error: return None | Success: return (operands, operators, result)
 def read_file(filename):
-    ## open input file
+    # open input file
     try:
         f = open("../files/input.txt", "r")
     except FileNotFoundError:
-        return None ## return None if there is an error in opening input file
+        return None # return None if there is an error in opening input file
 
-    ## variables for returning
+    # variables for returning
     operands = []
     operators = []
     result = ""
 
-    ## process input string
+    # process input string
     import re
     temp_string = f.readline()
     for item in re.split(r"([+-=*])", temp_string):
@@ -20,9 +21,34 @@ def read_file(filename):
             operands.append(item)
     result = operands.pop()
 
-    ## close file and return result
+    # close file and return result
     f.close()
-    return operands, operators, result ## successfully read file
+    return operands, operators, result # successfully read file
+
+# Error: return False | Success: return True
+def write_file(filename, assignment):
+    # create output file
+    try:
+        f = open("../files/output.txt", "w")
+    except:
+        return False # return False if there is an error in creating output file
+
+    result = []
+    for key, value in assignment.items():
+        result.append((key, value))
+    
+    def sort_with_key(item): # define sorting criteria
+        return item[0]
+    
+    result.sort(key=sort_with_key)
+
+    # write file
+    for item in result:
+        f.write(str(item[1]))
+
+    # close file
+    f.close()
+    return True # return True if creating file successfully
 
 def degree_heuristic(csp):
     var = None
