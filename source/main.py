9a982f31a0014b3cf5d9576bd666ceee6e74769c
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-07-23 02:18:23 +0700

    completed plus operator

--- source/main.py
+++ source/main.py
@@ -3,15 +3,13 @@ from utility_functions import *
 
 def solve(csp):
     if len(csp['variables']) > 10:
-        return {}
+        return None
 
-	infer_same_operand(csp)
-
-	assignment = {}
-	has_solution = backtracking(assignment, csp, len(csp.constraints) - 1, 0)
-	if has_solution:
-		return assignment
-    return
+    assignment = {}
+    has_solution = backtracking(assignment, csp, 0, 0)
+    if has_solution:
+        return assignment
+    return None
 
 
 if __name__ == '__main__':
@@ -19,12 +17,18 @@ if __name__ == '__main__':
     output_file_path = '../files/output.txt'
 
     data = read_file(input_file_path)
-    has_solution = False
     if data != None:
         csp = create_csp(data)
         assignment = solve(csp)
-        write_file(output_file_path)
-        has_solution = len(assignment) != 0
 
-    if not has_solution:
-        print("NO SOLUTION")
+        result = 'NO SOLUTION'
+        has_solution = (assignment != None)
+        if has_solution:
+            result = ''
+            for item in sorted(assignment.items()):
+                result += str(item[1])
+
+        write_file(output_file_path, result)
+        print(result)
+    else:
+        print('Cannot open:', input_file_path)
